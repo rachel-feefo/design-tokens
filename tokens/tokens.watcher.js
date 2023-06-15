9a62e68ec15120e5011f7fa60f2a721354d6f0dd
Rachel Wallis <103116493+rachel-feefo@users.noreply.github.com>
2023-06-15 12:01:14 +0100

    added a watcher to add $ to names

---
+++ tokens/tokens.watcher.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const inputFile = 'tokens.json';
+const outputFile = 'tokens.auto.json';
+
+// Watch for changes in the input file
+fs.watchFile(inputFile, (curr, prev) => {
+  // Read the contents of the input file
+  fs.readFile(inputFile, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    // Process the file contents and modify as needed
+    const updatedData = data.replace(/"value":/g, '"$value":').replace(/"type":/g, '"$type":');
+
+    // Write the modified contents to the output file
+    fs.writeFile(outputFile, updatedData, 'utf8', (err) => {
+      if (err) throw err;
+      console.log(`Successfully updated ${outputFile}`);
+    });
+  });
+});
